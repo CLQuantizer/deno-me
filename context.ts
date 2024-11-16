546a714e9e0a98e76bd69490143c08ad7e0f4da0
eziox <ezioxxk@gmail.com>
2024-11-16 22:58:53 +0100

    me

--- context.ts
+++ context.ts
@@ -2,6 +2,6 @@ import { BinarySearchTree } from '@std/data-structures/binary-search-tree';
 import {PriceLevel, Trade} from "./types.ts";
 
 export const BuyTree = new BinarySearchTree((a:PriceLevel, b) => a.price - b.price);
-export const SellTree = new BinarySearchTree((a:PriceLevel, b) => b.price - a.price);
+export const SellTree = new BinarySearchTree((a:PriceLevel, b) => a.price - b.price);
 
 export const Trades: Trade[] = [];
